74e65008bf908830b2650115af24b1219cad473f
Adams kehinde <kehindeadams78@gmail.com>
2024-06-12 02:49:39 -1100

    Update LoginSignUp.jsx

--- src/Component/LoginSignUp.jsx
+++ src/Component/LoginSignUp.jsx
@@ -44,9 +44,9 @@ const LoginSignUp = () => {
 
   return (
     <div className='m-5 p-5 card shadow '>
-        <div className="d-flex gap-5 mx-auto mb-3">
-            <div className={login ? 'h2 btn btn-secondary px-5' : 'h2'} onClick={()=>setLogin(true)}>Login</div>
-            <div className={!login ? 'h2 btn btn-secondary px-5' : 'h2'} onClick={()=>setLogin(false)}>SignUp</div>
+        <div className="d-flex gap-4 mx-auto mb-3">
+            <div className={login ? 'h2 btn btn-secondary px-3' : 'h2'} onClick={()=>setLogin(true)}>Login</div>
+            <div className={!login ? 'h2 btn btn-secondary px-3' : 'h2'} onClick={()=>setLogin(false)}>SignUp</div>
         </div>        <form onSubmit={(e)=>handleSubmit(e, login ? 'Login' : 'SignUp')}>
             <h1 className='text-center'>{login ? 'Login' : 'SignUp'}</h1>
             <div className="mb-3">
